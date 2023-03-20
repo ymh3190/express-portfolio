dbdd81ad3e6d3ec971fa16761490aed0d7186768
ymh3190 <ymh3190@gmail.com>
2023-03-20 17:27:24 +0900

    error-handler

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,18 +1,24 @@
 const { StatusCodes } = require("http-status-codes");
+const { CustomAPIError } = require("../errors");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  const customError = {
-    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || "Something went wrong try argain later",
-  };
-
-  if (err.code && err.errno === 1062) {
-    customError.msg = `${err.sqlMessage}, please choose another value`;
-    customError.statusCode = 400;
+  // mysql에서 validation 판단 안함
+  // const customError = {
+  //   statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+  //   msg: err.message || "Something went wrong try argain later",
+  // };
+  // if (err.code && err.errno === 1062) {
+  //   customError.msg = `${err.sqlMessage}, please choose another value`;
+  //   customError.statusCode = 400;
+  // }
+  if (err instanceof CustomAPIError) {
+    return res
+      .status(err.statusCode)
+      .render("pages/error", { msg: err.message });
   }
   res
-    .status(customError.statusCode)
-    .render("pages/error", { msg: customError.msg });
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .render("pages/error", { msg: err.message });
 };
 
 module.exports = errorHandlerMiddleware;

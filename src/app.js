cca1b2c8847caa796b5c21107bd1f789e8e8dad4
ymh3190 <ymh3190@gmail.com>
2023-03-23 17:44:08 +0900

    content security policy directive error ?

--- src/app.js
+++ src/app.js
@@ -1,25 +1,49 @@
 require("dotenv").config();
 require("./db/mysql");
+
+// security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 const session = require("express-session");
 const app = express();
+
+// session storage
 const MySQLStore = require("express-mysql-session")(session);
+
+// error handler
 const errorHandlerMiddleware = require("./middleware/error-handler");
-const localsMiddleware = require("./middleware/locals");
 const notFound = require("./middleware/notFound");
+
+// middleware
+const localsMiddleware = require("./middleware/locals");
+const publicOnlyMiddleware = require("./middleware/publicOnly");
+const privateOnlyMiddleware = require("./middleware/privateOnly");
+
+// routers
 const mainRouter = require("./routes/main");
 const userRouter = require("./routes/users");
 const videoRouter = require("./routes/videos");
 const oauthRouter = require("./routes/oauth");
 const historyRouter = require("./routes/history");
-const publicOnlyMiddleware = require("./middleware/publicOnly");
-const privateOnlyMiddleware = require("./middleware/privateOnly");
 
 app.set("trust proxy", 1);
 app.set("view engine", "ejs");
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15분
+    max: 100, // 윈도우당 100 요청 제한
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // session
 app.use(

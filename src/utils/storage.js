81edf0613cd2ff0abfe67071d501c379754c5fdd
ymh3190 <ymh3190@gmail.com>
2023-05-23 13:31:06 +0900

    modifying with chatgpt

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -7,11 +7,66 @@ import conn from "./ssh.js";
 class CustomAPIStorage {
   constructor(opts) {
     this.getDestination = opts.destination;
+
+    // ChatGPT
+    this.isConnected = false;
   }
 
   _handleFile(req, file, cb) {
     this.getDestination(req, file, (err, path) => {
       if (err) return cb(err);
+
+      /**
+       * 두번째 업로드부터 파일쓰기를 두번하는 문제가 발생하여
+       * ChatGPT를 통한 코드 수정
+       * DigitalOcean 사용기한 만료로 테스트 해보진 못함
+       */
+      // ChatGPT
+      const handleUpload = () => {
+        const hex = random();
+        const remotePath = `/mnt/volume_sgp1_01/${path}/${hex}`;
+        const outStream = conn.sftp((err, sftp) => {
+          if (err) return cb(err);
+
+          const outStream = sftp.createWriteStream(remotePath);
+          outStream.once("error", cb);
+          outStream.once("finish", () => {
+            conn.shell((err, stream) => {
+              if (err) return cb(err);
+              stream.on("exit", () => {
+                conn.end();
+              });
+              const command = `ln -s ${remotePath} /var/www/html/${path}/${hex}\nexit\n`;
+              stream.end(command);
+            });
+          });
+        });
+
+        file.stream.pipe(outStream);
+      };
+
+      conn.on("close", () => {
+        cb(null, {
+          path: `http://${process.env.DROPLETS_HOST}/${path}/${hex}`,
+          size: outStream.bytesWritten,
+        });
+      });
+
+      if (this.isConnected) {
+        handleUpload();
+      } else {
+        conn
+          .on("ready", () => {
+            this.isConnected = true;
+            handleUpload();
+          })
+          .connect({
+            host: process.env.DROPLETS_HOST,
+            username: process.env.DROPLETS_USER,
+            password: process.env.DROPLETS_PASSWORD,
+          });
+      }
+
       conn
         .on("ready", () => {
           conn.sftp((err, sftp) => {
